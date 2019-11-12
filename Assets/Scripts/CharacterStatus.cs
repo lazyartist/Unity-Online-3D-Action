af058ca0af570acef27ac9a543963090b2d690f8
a <a@a.a>
2019-11-12 15:33:33 +0900

    CharacterStatus

---
+++ Assets/Scripts/CharacterStatus.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStatus : MonoBehaviour {
+    //체렬
+    public int HP = 100;
+    public int MaxHp = 100;
+    //공격력
+    public int Power = 10;
+    //마지막에 공격한 대상
+    public GameObject lastAttackTarget = null;
+    //플레이어 이름
+    public string characterName = "Player";
+    //상태
+    public bool attacking = false;
+    public bool died = false;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
