561a0f7ed0a8c959f51e8c0a270b63c875ee2960
a <a@a.a>
2019-12-27 01:23:46 +0900

    game title scene

--- Assets/Scripts/GameRuleCtrl.cs
+++ Assets/Scripts/GameRuleCtrl.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameRuleCtrl : MonoBehaviour
 {
@@ -10,13 +11,26 @@ public class GameRuleCtrl : MonoBehaviour
     public bool gameOver = false;
     //게임 클리어 플래그
     public bool gameClear = false;
+    //씬 이행 시간
+    public float sceneChangeTime = 3.0f;
 
     void Update()
     {
+        if(gameOver || gameClear)
+        {
+            sceneChangeTime -= Time.deltaTime;
+            if(sceneChangeTime <= 0.0f)
+            {
+                SceneManager.LoadScene("TitleScene");
+                return;
+            }
+        }
+
         timeRemaining -= Time.deltaTime;
         //남은 시간이 없으면 게임 오버
         if(timeRemaining <= 0.0f)
         {
+            timeRemaining = 0.0f;
             GameOver();
         }
     }
---
+++ Assets/Scripts/TitleSceneCtrl.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleSceneCtrl : MonoBehaviour {
+    public void OnStartButtonClick()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+	
+}
